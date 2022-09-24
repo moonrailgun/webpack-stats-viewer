b373dd97aa08b1393ba6e3670f78434288f82afe
moonrailgun <moonrailgun@gmail.com>
2022-09-24 17:20:55 +0800

    chore: update publish detail

--- packages/webpack-plugin/src/webpack-plugin.ts
+++ packages/webpack-plugin/src/webpack-plugin.ts
@@ -69,7 +69,7 @@ const generateReports = async (
 
 export class WebpackStatsViewerPlugin {
   options: Options;
-  constructor(options: Options) {
+  constructor(options?: Options) {
     this.options = {
       ...DEFAULT_OPTIONS,
       ...options,
